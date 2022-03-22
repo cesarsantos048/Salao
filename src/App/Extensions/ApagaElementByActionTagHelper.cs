5d9d8f676105d82aa809fa518c8da77563b74c61
Antonio Cesar <cesarmassa1@hotmail.com>
2022-03-21 23:40:56 -0300

    ;

---
+++ src/App/Extensions/ApagaElementByActionTagHelper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Routing;
+using System;
+
+namespace App.Extensions
+{
+    [HtmlTargetElement("a", Attributes = "supress-by-action")]
+    public class ApagaElementByActionTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+        public ApagaElementByActionTagHelper(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        [HtmlAttributeName("supress-by-action")]
+        public string ActionName { get; set; }
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+            var action = _contextAccessor.HttpContext.GetRouteData().Values["action"].ToString();
+
+            if (ActionName.Contains(action)) return;
+
+            output.SuppressOutput();
+
+        }
+    }
+}
--- src/App/ViewModels/ServicoViewModel.cs
+++ src/App/ViewModels/ServicoViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App.Extensions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -18,15 +19,17 @@ namespace App.ViewModels
         [StringLength(200, ErrorMessage ="O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Nome { get; set; }
 
+        [Moeda]
         [Required(ErrorMessage = "O campo {0} é obrigatório!")]
         public decimal Valor { get; set; }
 
-        [DisplayName("Descrição")]
+        [DisplayName("Pagamento")]
         [Required(ErrorMessage = "O campo {0} é obrigatório!")]
         [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
 
         [ScaffoldColumn(false)]
+        [DisplayName("Data")]
         public DateTime DataCadastro { get; set; }
 
         public ClienteViewModel Cliente { get; set; }
